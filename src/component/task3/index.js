4ba6ef8c23e0f4e16a22c9402fde4bbbf7b0fb7f
Ольга Цыб <dev01@varius.local>
2021-05-26 14:22:41 +0300

    fix Task3

--- src/component/task3/index.js
+++ src/component/task3/index.js
@@ -1,18 +1,61 @@
+import React, { useState } from 'react';
+
+import Quiz from './quiz.json';
+import './styles.css';
+
 export const Task3 = () => {
+    const [input, setInput] = useState('')
+    const [nextStep, setNextStep] = useState('1')
+    const [result, setResult] = useState(false)
+    const [isDisabled, setDisabled] = useState(true);
+
+    const onInputChanged = (e)=> {
+        setInput(e.currentTarget.value) 
+        setDisabled(false)
+
+    }
+    
+    const handleClick = (e) => {
+        e.preventDefault();
+        if (input === "") {
+            setResult(true)
+        }
+        setNextStep(input)
+        setDisabled(true)
+    }
     return(
         <div className='container'>
-            <h1>Опросник</h1>
-            <form id="vote" action="#" method="POST">
+            <h1>Опросник</h1>     
+            {result
+            ? (<p>Спасибо за прохождение опросника</p>   ):(    
+            <form onSubmit={handleClick}>
                 <div id="content">
-                    <div class="pattern">
-                        <h4 class="question">Вопрос</h4>
-                        <div class="answers">
-                            <div class="radio"><label><input type="radio" name="poll"/>Вопрос</label></div>
+                    <div className="pattern">
+                        <h4 className="question">{Quiz[nextStep].name}</h4>
+                        <div className="answers">
+                        {Quiz[nextStep].answers.map((index, key)=>(
+                             <div className="radio" key={key}>
+                               
+                                     <input 
+                                        type="radio" 
+                                        name="value"
+                                        value={index.idNext}
+                                        onChange={onInputChanged} 
+                                    />
+                                        {index.name}
+
+                            </div>
+                        ))}
                         </div>
                     </div>
                 </div>
-            <button type="submit" class="btn btn-default">Ответить</button>
-            </form>
+            <button 
+                type="submit"
+                disabled={isDisabled}
+            >
+                Ответить
+            </button>
+            </form>  )}
         </div>
     )
 }
